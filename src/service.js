18f946a2be6610221ea835b75eaa2d5acb8a9c14
ShreeNaathRS <rsshreenaath@gmail.com>
2025-10-03 18:30:09 +0530

    service base url picked from env

--- src/service.js
+++ src/service.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
 export const productAxios = axios.create({
-    // baseURL: `https://ec2-13-201-97-47.ap-south-1.compute.amazonaws.com:9000/swift-cart`,
     baseURL: import.meta.env.VITE_API_URL
 })
-
-export const localAxios = axios.create({
-    baseURL: 'http://localhost:9000/swift-cart',
-})
-
