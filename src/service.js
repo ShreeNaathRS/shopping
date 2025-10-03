4765e492dea747b9756773ce685d3b4a735369a8
ShreeNaathRS <rsshreenaath@gmail.com>
2025-10-03 18:21:01 +0530

    .env.production added

--- src/service.js
+++ src/service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const productAxios = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/swift-cart`
+    // baseURL: `https://ec2-13-201-97-47.ap-south-1.compute.amazonaws.com:9000/swift-cart`,
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const localAxios = axios.create({
